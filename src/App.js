04b53fe0852da7d3c44fc3d8d6271ac081f3d4b0
torinmo adebayo <torinmo.work@gmail.com>
2023-05-03 23:38:15 +0100

    form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { validateEmail } from "./utils";
+
+const PasswordErrorMessage = () => {
+   // if(passwordInput.value.trim() === ''){
+    //     setError(passwordInput, 'Password can not be empty');
+    // }else if(passwordInput.value.trim().length <6 || passwordInput.value.trim().length >20){
+    //     setError(passwordInput, 'Password min 6 max 20 charecters');
+    // }else {
+    //     setSuccess(passwordInput);
+    // }
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => { 
+    return ( 
+      firstName && 
+      validateEmail(email) && 
+      password.value.length >= 8 && 
+      role !== "role" 
+    ); 
+  }; 
+
+  const clearForm = () => { 
+    setFirstName(""); 
+    setLastName(""); 
+    setEmail(""); 
+    setPassword({ 
+      value: "", 
+      isTouched: false, 
+    }); 
+    setRole("role"); 
+  }; 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account created!");
+    clearForm();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            {/* value & onchange makes it a controlled form component */}
+            <input placeholder="First name"
+             value={firstName} 
+             onChange={(e) =>{setFirstName(e.target.value);}}/>
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input placeholder="Last name"
+             value={lastName} 
+             onChange={(e) =>{setLastName(e.target.value);}}/>
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            {/* value & onchange makes it a controlled form component */}
+            <input placeholder="Email address" 
+            value={email} 
+            onChange={(e) =>{setEmail(e.target.value);}} />
+          </div>
+          <div className="Field"> 
+           <label> 
+             Password <sup>*</sup> 
+           </label> 
+           <input 
+             value={password.value} 
+             type="password" 
+             onChange={(e) => { 
+               setPassword({ ...password, value: e.target.value }); 
+             }} 
+             onBlur={() => { 
+               setPassword({ ...password, isTouched: true }); 
+             }} 
+             placeholder="Password" 
+           /> 
+           {password.isTouched && password.value.length < 8 ? ( 
+             <PasswordErrorMessage /> 
+           ) : null} 
+         </div> 
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={(e) =>{setRole(e.target.value);}}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 }
